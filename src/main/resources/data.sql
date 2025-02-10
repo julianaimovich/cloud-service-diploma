c7f0b0cd7d73ceb45d6865eb60bd54318dd811a7
najmovich <najmovich@protei.ru>
2025-02-10 06:32:57 +0300

    Application.properties fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT IGNORE INTO users (login, password) VALUES ('boss', '{noop}bosspasswd');
-INSERT IGNORE INTO users (login, password) VALUES ('admin', '{noop}adminpasswd');
-INSERT IGNORE INTO authorities (login, authority) VALUES ('boss', 'ROLE_ADMIN');
-INSERT IGNORE INTO authorities (login, authority) VALUES ('admin', 'ROLE_ADMIN');
+INSERT INTO users (login, password) VALUES ('boss', '{noop}bosspasswd');
+INSERT INTO users (login, password) VALUES ('admin', '{noop}adminpasswd');
+INSERT INTO authorities (login, authority) VALUES ('boss', 'ROLE_ADMIN');
+INSERT INTO authorities (login, authority) VALUES ('admin', 'ROLE_ADMIN');
